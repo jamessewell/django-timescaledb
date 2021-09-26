96f7bafc45ed7b26a2aab4d0292e6b3ea597225e
Rasmus Schlünsen <rasmus@schlunsen.com>
2021-09-26 12:44:21 +0200

    Started on base test for time bucket offset testing

--- example/metrics/tests.py
+++ example/metrics/tests.py
@@ -1,6 +1,18 @@
 from django.test import TestCase
+from metrics.models import Metric
+from datetime import datetime
+import pandas as pd
+import pytz
 
-class TimeBucketTest(TestCase):
 
+class TimeBucketTestCase(TestCase):
     def setUp(self):
-        print("Setup")
+        # Create sample data
+        timestamps = pd.date_range(end = datetime.today(), periods = 100).to_pydatetime().tolist()
+        for timestamp in timestamps:
+            Metric.objects.create(time=pytz.utc.localize(timestamp))
+    
+    def test_time_bucket_offset(self):
+        assert('INTERVAL' in str(Metric.timescale.filter(time__range=('2021-08-30', '2021-11-30')).time_bucket('time', "1 hour", offset="-1 hour").query))
+        
+        
